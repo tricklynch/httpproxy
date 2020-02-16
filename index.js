dc0012270f413b5bc02e0e65091c544724d7f7a3
tricklynch <p@tricklynch.org>
2020-02-16 12:36:48 -0500

    I'm not proud of this code, but it has comments

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ proxy.on('connection', client_sock => {
         const client_request = client_data.toString('ascii')
         console.log('c>p ' + client_request.replace(/\n/g, '\nc>p '))
         const match = /([^\s]+) ([^\s]+) ([^\s]+)/.exec(client_request)
+        // TODO Figure out if this is the right way to handle CONNECT
         if('CONNECT' === match[1]) {
             if(client_sock.writable) {
                 client_sock.end(`${match[3]} 200 OK\r\n\r\n`)
             }
         }
-        const href = url.parse(match[2])
-        const port = null === href.port
-            ? 'http:' === href.protocol
-                ? 80
-                : 443
-            : href.port
-        const host = href.href.startsWith('http')
-            ? href.hostname
-            : href.href.slice(0, href.href.length - href.hostname.length - 1)
+        const req_url = url.parse(match[2])
+        // Port and host are required to create a connection
+        // Gets around a bug(?) in URL module
+        // When there's a port but no protocol,
+        // the protocol is the hostname
+        // the hostname is the port
+        const port = null === req_url.port
+            ? req_url.host
+            : req_url.port
+        const host = req_url.href.startsWith('http')
+            ? req_url.hostname
+            : req_url.protocol.slice(0, req_url.protocol.length - 1)
         const server_sock = server_agent.createConnection(port, host)
         server_sock.on('error', console.log)
+        // Forward the server's response to the client
         server_sock.on('data', server_data => {
             console.log('s>p ' + server_data.toString('ascii').replace(/\n/g, '\ns>p '))
             if(client_sock.writable) {
@@ -38,6 +43,7 @@ proxy.on('connection', client_sock => {
             /[A-Z]+ [^ ]+ HTTP\/\d\.\d/,
             `${match[1]} ${path} ${match[3]}`
         ).replace(/Proxy-Connection: .*\r\n/, '')
+        // Forward the client's request to the server
         if(server_sock.writable) {
             console.log('p>s ' + server_request.replace(/\n/g, '\np>s '))
             server_sock.write(Buffer.from(server_request))
