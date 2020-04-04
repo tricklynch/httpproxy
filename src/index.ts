f21806d1a39fc7e996fcfbd876f79b6335441543
tricklynch <p@tricklynch.org>
2020-04-04 16:01:11 -0400

    Converted to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-const http = require('http')
-const url = require('url')
+import { Agent, Server } from 'http'
+import { parse } from 'url'
 
-const DEBUG = false
+const DEBUG = true
 
-function debug_print(msg) {
-    if(DEBUG) debug_print(msg)
+function debug_print(msg: string) {
+    if(DEBUG) console.log(msg)
 }
 
-const proxy = new http.Server()
+const proxy = new Server()
 
 proxy.on('connection', client_sock => {
     client_sock.on('error', debug_print)
-    const server_agent = new http.Agent()
+    const server_agent = new Agent()
     client_sock.on('data', client_data => {
         const client_request = client_data.toString('ascii')
         debug_print(`${+new Date()}\nc>p ${client_request.replace(/\n/g, '\nc>p ')}`)
         const match = /([^\s]+) ([^\s]+) ([^\s]+)/.exec(client_request)
+        if(null === match || undefined === match.length || 4 > match.length) {
+            throw new Error('Invalid HTTP request line')
+        }
         // TODO Figure out if this is the right way to handle CONNECT
         if('CONNECT' === match[1]) {
             if(client_sock.writable) {
@@ -23,20 +26,16 @@ proxy.on('connection', client_sock => {
             }
             return
         }
-        const req_url = url.parse(match[2])
-        // Port and host are required to create a connection
-        // Gets around a bug(?) in URL module
-        // When there's a port but no protocol,
-        // the protocol is the hostname
-        // the hostname is the port
+        const req_url = parse(match[2])
         const port = null === req_url.port
             ? 'http:' === req_url.protocol
                 ? 80
                 : 443
-            : req_url.port
-        const host = req_url.href.startsWith('http')
-            ? req_url.hostname
-            : req_url.protocol.slice(0, req_url.protocol.length - 1)
+            : +req_url.port
+        const host = req_url.hostname
+        if(null === host) {
+            throw new Error('Invalid URL')
+        }
         const server_sock = server_agent.createConnection(port, host)
         server_sock.on('error', debug_print)
         // Forward the server's response to the client
@@ -47,7 +46,7 @@ proxy.on('connection', client_sock => {
                 client_sock.write(server_data)
             }
         })
-        const path = url.parse(match[2]).path || '/'
+        const path = parse(match[2]).path || '/'
         const server_request = client_request.replace(
             /[A-Z]+ [^ ]+ HTTP\/\d\.\d/,
             `${match[1]} ${path} ${match[3]}`
